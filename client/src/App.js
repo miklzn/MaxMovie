508e32a926f0595b76e78564cb15bcf16c3d10d9
miklzn <mikelzonsarabia@gmail.com>
2023-02-02 10:40:31 -0300

    Tailwind configurado

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Movies from "./components/Movies";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Movies />} />
       </Routes>
